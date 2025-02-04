4415f8ee547707cbf42425f6ab44e912c0e5f468
DinooSaaw <57213778+DinooSaaw@users.noreply.github.com>
2025-02-04 23:02:22 +1100

    Add guild settings check to balance command for user visibility control

--- commands/balance.js
+++ commands/balance.js
@@ -1,21 +1,48 @@
-const { SlashCommandBuilder, EmbedBuilder, MessageFlags } = require('discord.js');
-const User = require('../Models/User.js');
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  MessageFlags,
+} = require("discord.js");
+const User = require("../Models/User.js");
+const GuildSettings = require("../models/Settings.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('balance')
-        .setDescription('Check your gold balance.'),
-    async execute(interaction) {
-        let user = await User.findOne({ _id: interaction.user.id });
-        if (!user) user = await User.create({ _id: interaction.user.id, name: interaction.user.username, gold: 10 });
-
-        const balanceEmbed = new EmbedBuilder()
-            .setColor('#FFD700') // Gold color
-            .setTitle('💰 Balance Check')
-            .setDescription(`**${interaction.user.displayName}**, you have **${user.gold.toLocaleString()}** gold.`)
-            .setThumbnail(interaction.user.displayAvatarURL({ dynamic: true }))
-            .setFooter({ text: 'Use /salary to collect your salary!', iconURL: interaction.client.user.displayAvatarURL() });
-
-        await interaction.reply({ embeds: [balanceEmbed] });
+  data: new SlashCommandBuilder()
+    .setName("balance")
+    .setDescription("Check your gold balance."),
+  async execute(interaction) {
+    let settings = await GuildSettings.findOne({ _id: interaction.guild.id });
+    if (!settings) {
+      settings = await GuildSettings.create({
+        _id: interaction.guild.id,
+      });
     }
+    let user = await User.findOne({ _id: interaction.user.id });
+    if (!user)
+      user = await User.create({
+        _id: interaction.user.id,
+        name: interaction.user.username,
+        gold: 10,
+      });
+
+    const balanceEmbed = new EmbedBuilder()
+      .setColor("#FFD700") // Gold color
+      .setTitle("💰 Balance Check")
+      .setDescription(
+        `**${
+          interaction.user.displayName
+        }**, you have **${user.gold.toLocaleString()}** gold.`
+      )
+      .setThumbnail(interaction.user.displayAvatarURL({ dynamic: true }))
+      .setFooter({
+        text: "Use /salary to collect your salary!",
+        iconURL: interaction.client.user.displayAvatarURL(),
+      });
+
+    if(settings.showUsersBalance){
+        await interaction.reply({ embeds: [balanceEmbed] });
+    } else {
+        await interaction.reply({ embeds: [balanceEmbed], flags: MessageFlags.Ephemeral });
+    }
+  },
 };
