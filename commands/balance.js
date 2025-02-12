ab08d7358b72547ff16bc84b46d8c0434da81bc3
DinooSaaw <57213778+DinooSaaw@users.noreply.github.com>
2025-02-12 18:24:20 +1100

    Add job model import and associate job data with new users in balance command

--- commands/balance.js
+++ commands/balance.js
@@ -4,6 +4,7 @@ const {
   MessageFlags,
 } = require("discord.js");
 const User = require("../models/User.js");
+const Job = require("../models/Job.js"); // Import Job model
 const GuildSettings = require("../models/Settings.js");
 
 module.exports = {
@@ -17,20 +18,25 @@ module.exports = {
         _id: interaction.guild.id,
       });
     }
+
+    // Retrieve user data
     let user = await User.findOne({ _id: interaction.user.id });
-    if (!user)
+    if (!user) {
+      var job = await Job.findOne({ roleId: { $in: interaction.member.roles.cache.map(role => role.id) } });
       user = await User.create({
         _id: interaction.user.id,
         name: interaction.user.username,
+        job: job ? job.name : null,
       });
+    }
+
+    // Check if the user has a job and if the job's role matches
 
     const balanceEmbed = new EmbedBuilder()
       .setColor("#FFD700") // Gold color
       .setTitle("💰 Balance Check")
       .setDescription(
-        `**${
-          interaction.user.displayName
-        }**, you have **${user.gold.toLocaleString()}** gold.`
+        `**${interaction.user.displayName}**, you have **${user.gold.toLocaleString()}** gold.`
       )
       .setThumbnail(interaction.user.displayAvatarURL({ dynamic: true }))
       .setFooter({
@@ -38,10 +44,11 @@ module.exports = {
         iconURL: interaction.client.user.displayAvatarURL(),
       });
 
-    if(settings.showUsersBalance){
-        await interaction.reply({ embeds: [balanceEmbed] });
+    // Check the setting for showing user balance
+    if (settings.showUsersBalance) {
+      await interaction.reply({ embeds: [balanceEmbed] });
     } else {
-        await interaction.reply({ embeds: [balanceEmbed], flags: MessageFlags.Ephemeral });
+      await interaction.reply({ embeds: [balanceEmbed], flags: MessageFlags.Ephemeral });
     }
   },
 };
