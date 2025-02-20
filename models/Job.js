5784cbccac61ea972cf643f404d0a1c0a082d18b
DinooSaaw <57213778+DinooSaaw@users.noreply.github.com>
2025-02-20 17:30:37 +1100

    Remove roleId field from job schema as it is no longer needed

--- models/Job.js
+++ models/Job.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const jobSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
     basePay: { type: Number, required: true },
-    roleId: { type: String, required: false },
+    // roleId: { type: String, required: false },
     tax: { type: Number, required: false, default: 0 }
 });
 
