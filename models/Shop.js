a78ed743c861a1aa63775a55a7774dd0c5526265
DinooSaaw <57213778+DinooSaaw@users.noreply.github.com>
2025-03-01 21:34:25 +1100

    Set default values for wallet and salary fields in Shop schema

--- models/Shop.js
+++ models/Shop.js
@@ -3,14 +3,14 @@ const mongoose = require("mongoose");
 const ShopSchema = new mongoose.Schema({
   name: String, // Shop Name
   owner: { type: String, required: true }, // Owner's Name
-  wallet: Number, // Gold balance of the shop
-  salary: Number, // Amount the shop receives when claimed
+  wallet: { type: Number, default: 0 }, // Gold balance of the shop
+  salary: { type: Number, default: 0 }, // Amount the shop receives when claimed
   maxEmployees: Number, // Maximum number of employees the shop can have
   staff: [
     {
       name: String, // Staff Member's Name
       id: String, // Staff Member's ID
-      salary: Number, // Salary they receive from shop
+      salary: Number, // Salary they receive from the shop
     },
   ],
   inventory: [
